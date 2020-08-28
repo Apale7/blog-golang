592792c82130f7085df80bce7dc7fb7452072447
Apale7 <1092377056@qq.com>
2020-08-28 18:11:04 +0800

    dbconfig

--- go.mod
+++ go.mod
@@ -4,6 +4,9 @@ go 1.14
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/jinzhu/gorm v1.9.16
+	github.com/mitchellh/mapstructure v1.1.2
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.6.0
+	github.com/spf13/viper v1.7.1
 )
--- main.go
+++ main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/pkg/errors"
-	log "github.com/sirupsen/logrus"
-	"os"
+	_ "blog-gin/db"
 )
 
 func main() {
-	_, err := os.Open("go.mod1")
-	if err != nil {
-		err = errors.WithStack(err)
-		log.Warning(err)
-	}
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
